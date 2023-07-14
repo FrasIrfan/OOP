660a40147e628fb94a5528c8a2d7bb9234824cdc
FrasIrfan <leofras0051@gmail.com>
2023-07-15 02:24:13 +0500

    Object Oriented Programming

---
+++ overloading_binary_operator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+class Add
+{
+private:
+    int a, b;
+
+public:
+    Add()
+    {
+        a = b = 0;
+    }
+    void input()
+    {
+        cout << "Enter a: ";
+        cin >> a;
+        cout << "Enter b: ";
+        cin >> b;
+    }
+    void output()
+    {
+        cout << "The value of a = " << a << endl;
+        cout << "The value of b = " << b << endl;
+    }
+    Add operator+(Add p)
+    {
+        Add temp;
+        temp.a = a - p.a;
+        temp.b = b - p.b;
+        return temp;
+    }
+};
+
+int main()
+{
+    Add obj1, obj2, obj3;
+    obj1.input();
+    obj2.input();
+    obj3 = obj1 + obj2;
+    obj1.output();
+    obj2.output();
+    obj3.output();
+
+    return 0;
+}
---
+++ virtual_function.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+class A
+{
+public:
+    virtual void show()
+    {
+        cout << "Parent Class A" << endl;
+    }
+};
+class B : public A
+{
+public:
+    void show()
+    {
+        cout << "Child Class B" << endl;
+    }
+};
+class C : public A
+{
+public:
+    void show()
+    {
+        cout << "Child Class C" << endl;
+    }
+};
+
+int main()
+{
+    A obj1;
+    B obj2;
+    C obj3;
+    A *ptr; //A pointer ptr of type A is declared. This pointer can be used to point to objects of both the base class A and its derived classes.
+    ptr = &obj1;
+    ptr->show();
+    ptr = &obj2;
+    ptr->show();
+    ptr = &obj3;
+    ptr->show();
+    return 0;
+}
