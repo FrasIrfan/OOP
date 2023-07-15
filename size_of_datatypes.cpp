7ccf6cfa1cc8fc74dacbb7df66106610286af858
FrasIrfan <leofras0051@gmail.com>
2023-07-15 15:21:33 +0500

    size of all the data types

--- size_of_datatypes.cpp
+++ size_of_datatypes.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()
 {
+    
     cout << "Size of char:" << sizeof(char) << endl;
     cout << "Size of int:" << sizeof(int) << endl;
     cout << "Size of float:" << sizeof(float) << endl;
