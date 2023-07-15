256256260151cffb940fcd647ac3a17499589db6
FrasIrfan <leofras0051@gmail.com>
2023-07-15 14:44:12 +0500

    performing a type conversion in a single line of code

--- explicit_type_casting.cpp
+++ explicit_type_casting.cpp
@@ -5,7 +5,7 @@ int main()
 {
     float x = 10.3, y = 5.2;
     float ans;
-    ans = (int)x % (int)y; // Conversion of data type
+    ans = (int)x % (int)y; //CONVERSION OF DATA TYPE
     cout << ans << endl;
     cout << sizeof(ans);
     return 0;
