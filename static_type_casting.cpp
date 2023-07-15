c1ebc25e3bc6df5386a0ae43545641367bf2482b
FrasIrfan <leofras0051@gmail.com>
2023-07-15 15:22:59 +0500

    a way to convert a value from one data type to another at compile-time.

--- static_type_casting.cpp
+++ static_type_casting.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int num1 = 10;
-    double num2 = static_cast<double>(num1);
+    double num2 = static_cast<double>(num1); //a way to convert a value from one data type to another at compile-time.
     cout << "Num 1: " << num1 << endl;
     cout << "Num 2: " << num2 << endl;
     return 0;
