9c2de12a029c4b9bd4f4c7eb81c149a2f4756a5d
FrasIrfan <leofras0051@gmail.com>
2023-07-15 14:53:20 +0500

    addition of two increment operator variables

--- increrment_operations.cpp
+++ increrment_operations.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main()
 {
     int x = 20, y = 35;
-    x = y++ + x++;
-    y = ++y + ++x;
+    x = y++ + x++;  // addition to two increment operator variable
+    y = ++y + ++x;  // vice versa
     cout << x << endl;
     cout << y << endl;
     return 0;
