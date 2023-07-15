c1fc2deb94ba284b391e4f1f5568130108ea5c36
FrasIrfan <leofras0051@gmail.com>
2023-07-15 14:51:58 +0500

    increment operation being applied with multiplication operator

--- increment_operations_1.cpp
+++ increment_operations_1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int a = 3, b = 4;
-    ++a *= ++b;
+    ++a *= ++b; // increment operation being applied with multiplication operator
     cout << a << endl;
     return 0;
 }
