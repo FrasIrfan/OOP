a7b6656daad03432e0280ad8a8147bac0b2b0d76
FrasIrfan <leofras0051@gmail.com>
2023-07-15 15:19:46 +0500

    sets the width of the field for the output of values in formatted output streams.

--- setw_manipulator.cpp
+++ setw_manipulator.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     int a;
-    cout << setw(10) << "Number";
+    cout << setw(10) << "Number"; //sets the width of the field for the output of values in formatted output streams.
     cout << setw(10) << "Square";
     cout << setw(10) << "Cube";
 
