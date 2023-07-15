10db3589a663bfaee68e975c92639ed2ae677c41
FrasIrfan <leofras0051@gmail.com>
2023-07-15 14:49:05 +0500

    postflix and prefix increment operator

--- increment.cpp
+++ increment.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     int i = 5;
-    cout << i << endl;
-    cout << i++ << endl;
-    cout << i << endl;
-    cout << ++i << endl;
+    cout << i << endl;  
+    cout << i++ << endl; // postflix increment
+    cout << i << endl; // updated value
+    cout << ++i << endl; //prefix increment
     return 0;
 }
