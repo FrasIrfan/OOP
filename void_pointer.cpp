7acd059890a6cb635600d8c1dd3d6e9ad9769c93
FrasIrfan <leofras0051@gmail.com>
2023-07-15 20:37:11 +0500

    a type of pointer that can store the address of any type of variable

---
+++ void_pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    float f = 25.18;
+    char c = '$';
+    void *ptr;
+    ptr = &n;
+    cout << "The value of n: " << n << endl;
+    cout << "The address of n: " << ptr << endl;
+    ptr = &f;
+    cout << "The value of f: " << f << endl;
+    cout << "The address of f: " << ptr << endl;
+    ptr = &c;
+    cout << "The value of c: " << c << endl;
+    cout << "The address of c: " << ptr << endl;
+    return 0;
+}
