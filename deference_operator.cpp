31ca8cbbc110a550743de747ab6644eebebad63d
FrasIrfan <leofras0051@gmail.com>
2023-07-15 22:44:30 +0500

    deference operator is used to access the value of the variable whose memory address is stored in pointer~

--- deference_operator.cpp
+++ deference_operator.cpp
@@ -4,6 +4,8 @@ using namespace std;
 int main()
 {
     //deference operator is used to access the value of the variable whose memory address is stored in pointer.
+    //The need to use the dereference operator arises when you have a pointer variable pointing to a memory address
+    //and you want to access or modify the value stored at that memory location directly.
     int a, b, s, *p1, *p2;
     p1 = &a;
     p2 = &b;
