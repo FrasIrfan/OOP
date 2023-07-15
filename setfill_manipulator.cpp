df82104a697ee4b48f2e8f26c4bb904db0e4ea7f
FrasIrfan <leofras0051@gmail.com>
2023-07-15 15:14:49 +0500

    used to set the fill character for output streams.

--- setfill_manipulator.cpp
+++ setfill_manipulator.cpp
@@ -6,7 +6,7 @@ int main()
 {
     string str = "OOP Using C++";
     cout << "SETFILL MANIPULATOR" << endl;
-    cout << setw(20) << setfill('*') << str << endl;
+    cout << setw(20) << setfill('*') << str << endl; //used to set the fill character for output streams.
     cout << setw(25) << setfill('#') << str << endl;
     cout << setw(30) << setfill('@') << str << endl;
 
