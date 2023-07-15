2a4ba6d8cf49ef15b0365249c57e63e3cd515677
FrasIrfan <leofras0051@gmail.com>
2023-07-15 15:17:24 +0500

    sets the number of decimal places to display for floating-point values in output streams.

--- setprecision_manipulator.cpp
+++ setprecision_manipulator.cpp
@@ -8,7 +8,7 @@ int main()
     r = n1 / n2;
     cout << r << endl;
     cout << "SET PRECISION" << endl;
-    cout << setprecision(5) << r << endl;
+    cout << setprecision(5) << r << endl; //sets the number of decimal places to display for floating-point values in output streams.
     cout << setprecision(4) << r << endl;
     cout << setprecision(3) << r << endl;
     cout << setprecision(2) << r << endl;
