0bdc35aad4e64e2cfa9de941ffc1eb1b99cd1802
FrasIrfan <leofras0051@gmail.com>
2023-07-15 19:58:43 +0500

    reference operator and address operator

---
+++ address_of_variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    cout << "The value of n:" << n << endl;
+    cout << "The address of n:" << &n << endl;
+    return 0;
+}
