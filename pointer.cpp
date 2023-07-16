3c9bd28fac1062079c58dc12e3338ceac85c77dd
FrasIrfan <leofras0051@gmail.com>
2023-07-16 22:22:24 +0500

    A pointer is a variable that stores the memory address of another variable

--- pointer.cpp
+++ pointer.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     int n;
-    int *ptr;
+    int *ptr; // A pointer is a variable that stores the memory address of another variable
     cout << "Enter an integer: ";
     cin >> n;
     ptr = &n;
