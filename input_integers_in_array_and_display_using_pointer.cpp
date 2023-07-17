b5b0f120136e9053f417373158b2e6ad3c0c7058
FrasIrfan <leofras0051@gmail.com>
2023-07-18 01:51:25 +0500

    input integers and display using pointer

--- input_integers_in_array_and_display_using_pointer.cpp
+++ input_integers_in_array_and_display_using_pointer.cpp
@@ -5,17 +5,16 @@ int main()
 {
     int Num[5];
     int *ptr;
-    for (int i = 0; i <= 4; i++)
+    for (int i = 0; i < 5; i++)
     {
         cout << "Enter the value of Num[" << i << "]: ";
         cin >> Num[i];
     }
     ptr = Num;
     cout << "You entered the following values:\n";
-    for (int i = 0; i <= 4; i++)
+    for (int i = 0; i < 5; i++)
     {
         cout << *ptr++ << "\t";
-        ptr++;
     }
     return 0;
 }
