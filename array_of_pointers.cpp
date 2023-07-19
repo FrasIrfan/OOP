8949565b1367f87dcc8d9db3e6eeea60de7a48b2
FrasIrfan <leofras0051@gmail.com>
2023-07-19 17:40:41 +0500

    an array of pointers is an array in which each element is a pointer

--- array_of_pointers.cpp
+++ array_of_pointers.cpp
@@ -4,14 +4,13 @@ using namespace std;
 int main()
 {
     int *ptr[3], a, b, c;
-    int i;
     ptr[0] = &a;
     ptr[1] = &b;
     ptr[2] = &c;
     cout << "Enter three Integers: " << endl;
     cin >> a >> b >> c;
     cout << "The values are: " << endl;
-    for (i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
         cout << *ptr[i] << endl;
     }
