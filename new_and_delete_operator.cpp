9907a741ebf9a3f5306cb3c658fc8d76c3a81993
FrasIrfan <leofras0051@gmail.com>
2023-07-22 22:31:47 +0500

    memory is allocated using the new operator to store an integer on the heap. The heap is a region of memory where dynamic memory allocation takes place. When you allocate memory with new, it is your responsibility to deallocate it when it is no longer needed using the delete operator. Failing to deallocate memory properly can lead to a memory leak

--- new_and_delete_operator.cpp
+++ new_and_delete_operator.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
     int *ptr;
-    ptr = new int;
+    ptr = new int; //memory is allocated using the new operator to store an integer on the heap.
     cout << "Enter a Integer: ";
     cin >> *ptr;
     cout << "You entered: " << *ptr << endl;
     cout << "Is is stored at " << ptr << endl;
-    delete ptr;
+    delete ptr; // delete operator is used to avoid MEMORY LEAK 
     cout << "Is is stored at " << ptr << endl;
 
     return 0;
